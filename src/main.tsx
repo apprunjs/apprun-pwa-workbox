305f5636a7bd9418cce5989380a079e8eb415456
Yiyi Sun <yiyisun@gmail.com>
2020-02-26 11:32:08 -0500

    add sw scope

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import Contact from './Contact';
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker
-      .register("sw.js")
+      .register("sw.js", {scope: './'})
       .then(registration => {
         console.log("SW registered: ", registration);
       })
